e65717b3d3b5dcdec91998fa0b513951f4a708bb
anderson-br-ti <80790139+anderson-br-ti@users.noreply.github.com>
2021-06-10 16:29:13 -0300

    Continuação

--- js/listaTarefas.js
+++ js/listaTarefas.js
@@ -1,1 +1,11 @@
-document.querySelector('')
+const criarTarefa = (evento) => {
+  evento.preventDefault();
+  const input = document.querySelector('[data-form-input]');
+  const valor = input.value;
+  console.log(valor);
+  input.value = " ";
+}
+
+const novaTarefa = document.querySelector('[data-form-button]');
+
+novaTarefa.addEventListener('click', criarTarefa);
